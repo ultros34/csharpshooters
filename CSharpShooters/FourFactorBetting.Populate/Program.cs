bf285f2166e4866172959e98eadd2a2a86bd719f
ultros34 <ericklein34@gmail.com>
2019-12-04 22:39:25 -0500

    Initial solution & console application check in

---
+++ CSharpShooters/FourFactorBetting.Populate/Program.cs
@@ -0,0 +1,18 @@
+﻿using FourFactorBetting.Populate.Dtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace FourFactorBetting.Populate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage result = httpClient.GetAsync("http://data.nba.net/10s/prod/v1/today.json").Result;
+
+            NbaRoot root = JsonConvert.DeserializeObject<NbaRoot>(result.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
